40a840450e146dffdc31267fddf4a372fa654fe7
saketkarnik79 <78717476+saketkarnik79@users.noreply.github.com>
2025-01-29 09:10:47 +0530

    Add files via upload

---
+++ DNC_DemoMVCWebAppWithData/DNC_DemoMVCWebAppWithData/Controllers/PeopleController.cs
@@ -0,0 +1,22 @@
+﻿using DNC_DemoMVCWebAppWithData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNC_DemoMVCWebAppWithData.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Details()
+        {
+            Person person = new Person()
+            {
+                Id = 1,
+                Name = "Foo",
+                DateOfBirth = DateTime.Parse("19-Jan-1970"),
+                Email = "foo@bar.com"
+            };
+            //ViewData.Model = person;
+            //return View();
+            return View(person);
+        }
+    }
+}
