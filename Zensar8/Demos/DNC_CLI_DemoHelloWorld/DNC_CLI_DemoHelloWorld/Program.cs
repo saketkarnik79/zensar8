0455c1ca3ecf693fb13bdb030ebdac20a7c5f145
saketkarnik79 <78717476+saketkarnik79@users.noreply.github.com>
2025-01-29 09:08:59 +0530

    Add files via upload

---
+++ Zensar8/Demos/DNC_CLI_DemoHelloWorld/DNC_CLI_DemoHelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DNC_CLI_DemoHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
