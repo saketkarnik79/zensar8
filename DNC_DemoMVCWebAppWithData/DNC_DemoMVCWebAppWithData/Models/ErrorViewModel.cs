576a571c2727a8f0a6825147e17890d19b73a704
saketkarnik79 <78717476+saketkarnik79@users.noreply.github.com>
2025-01-29 09:13:39 +0530

    Delete DNC_DemoMVCWebAppWithData directory

--- DNC_DemoMVCWebAppWithData/DNC_DemoMVCWebAppWithData/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DNC_DemoMVCWebAppWithData.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
